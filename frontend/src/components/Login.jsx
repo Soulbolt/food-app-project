4d2ed4c4007c78f5a6d01c9177925926c7b8b262
Soulbolt <soulbolt@gmail.com>
2024-05-14 13:28:04 -0700

    Rename login backgorund img, add h2 title, label and intput for username.

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,5 +1,21 @@
-function login() {
-  return <div>Log In</div>;
-}
+import LoginImg from "../assets/login-healthy-options.jpg";
 
-export default login;
+export default function login() {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2">
+      <div>
+        <img src={LoginImg} alt="login" />
+      </div>
+
+      <div>
+        <form action="">
+          <h2>BRAND</h2>
+          <div>
+            <label htmlFor="">Username</label>
+            <input type="text" className="border border-slate-300" />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
