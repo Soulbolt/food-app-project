fb4774e7a5d4e0582a5d2833858625075813190f
Soulbolt <soulbolt@gmail.com>
2024-05-15 21:53:31 -0700

    Add login button, remember me check box, and create account button. Add styles to unify login content in a block. Add hover effect to login button.

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,18 +1,40 @@
+import React from "react";
 import LoginImg from "../assets/login-healthy-options.jpg";
 
 export default function login() {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2">
-      <div>
-        <img src={LoginImg} alt="login" />
+    <div className="grid h-screen w-full grid-cols-1 sm:grid-cols-2">
+      <div className="hidden sm:block">
+        <img
+          src={LoginImg}
+          alt="login"
+          className="h-full w-full object-cover"
+        />
       </div>
 
-      <div>
-        <form action="">
-          <h2>BRAND</h2>
-          <div>
-            <label htmlFor="">Username</label>
-            <input type="text" className="border border-slate-300" />
+      <div className="flex flex-col justify-center bg-gray-100">
+        <form
+          action="submit"
+          className="mx-auto w-full max-w-[400px] bg-white p-4"
+        >
+          <h2 className="py-6 text-center text-4xl font-bold">My Food</h2>
+          <div className="flex flex-col py-2">
+            <label>Username</label>
+            <input type="text" className="border border-slate-300 p-2" />
+          </div>
+          <div className="flex flex-col py-2">
+            <label>Password</label>
+            <input type="password" className="border border-slate-300 p-2" />
+          </div>
+          <button className="my-5 w-full border bg-indigo-600 py-2 text-white hover:bg-indigo-500">
+            Login
+          </button>
+          <div className="flex justify-between">
+            <p className="flex items-center">
+              <input type="checkbox" className="mr-2" />
+              Remember Me
+            </p>
+            <button>Create an account</button>
           </div>
         </form>
       </div>
