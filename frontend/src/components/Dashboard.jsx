f0baef942c635bdc6cc7a736d174579d14eafbdf
Soulbolt <soulbolt@gmail.com>
2024-05-18 19:36:31 -0700

    Add Welcome title to the page.

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -5,10 +5,14 @@ function Dashboard() {
   return (
     <div className="relative h-screen w-full bg-zinc-900/90">
       <img
-        className="absolute h-full w-full object-cover"
+        className="absolute h-full w-full object-cover mix-blend-overlay"
         src={LoginImg}
         alt="breakfast"
       />
+
+      <div>
+        <h2 className="text-5xl text-indigo-300">Welcome To Your Dashboard</h2>
+      </div>
     </div>
   );
 }
