4420df2768234d0c69ba7574eae06d0c097457e5
Soulbolt <soulbolt@gmail.com>
2024-05-18 19:27:45 -0700

    Fix type on css class, add breakfast img as background.

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import LoginImg from "../assets/breakfast.jpg";
 
 function Dashboard() {
   return (
     <div className="relative h-screen w-full bg-zinc-900/90">
       <img
-        className="abosulute h-full w-full object-cover"
-        src="https://source.unsplash.com/random"
-        alt=""
+        className="absolute h-full w-full object-cover"
+        src={LoginImg}
+        alt="breakfast"
       />
     </div>
   );
