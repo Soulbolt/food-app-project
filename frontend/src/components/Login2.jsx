2443a75fe622ae831a735760927dddbbc2aeafd0
Soulbolt <soulbolt@gmail.com>
2024-05-20 09:26:16 -0700

    Add styling to center and align text, add react-icons, add google and facebook icons. Add styling to icons to give them a shadowbox for a button look with a hover effect. Add styling to ussrname and password inputs for a more uniform look. Add styling with indigo color to the button element. Add styles to paragraph elements for a more uniform look.

--- frontend/src/components/Login2.jsx
+++ frontend/src/components/Login2.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import LoginImg from "../assets/breakfast.jpg";
+import { FcGoogle } from "react-icons/fc";
+import { AiFillFacebook } from "react-icons/ai";
+import LoginImg from "../assets/login-healthy-options.jpg";
 
 function Login2() {
   return (
@@ -13,24 +15,33 @@ function Login2() {
       <div className="flex h-full items-center justify-center">
         <form className="mx-auto w-full max-w-[400px] bg-white p-8">
           <h2 className="py-8 text-center text-4xl font-bold">My Food</h2>
-          <div>
-            <p>ICON. Facebook</p>
-            <p>ICON. Google</p>
+          <div className="flex justify-between py-8">
+            <p className="relative flex items-center border px-6 py-2 shadow-lg hover:shadow-xl">
+              <AiFillFacebook className="mr-2" /> Facebook
+            </p>
+            <p className="relative flex items-center border px-6 py-2 shadow-lg hover:shadow-xl">
+              <FcGoogle className="mr-2" /> Google
+            </p>
           </div>
-          <div>
-            <label>Username</label>
-            <input type="text" />
+          <div className="mb-4 flex flex-col">
+            <label className="flex justify-start">Username</label>
+            <input type="text" className="relative border bg-gray-100 p-2" />
           </div>
-          <div>
-            <label>Password</label>
-            <input type="password" />
+          <div className="flex flex-col">
+            <label className="flex justify-start">Password</label>
+            <input
+              type="password"
+              className="relative border bg-gray-100 p-2"
+            />
           </div>
-          <button>Sign In</button>
-          <p>
-            <input type="checkbox" />
+          <button className="relative mt-8 w-full bg-indigo-600 py-3 text-white hover:bg-indigo-500">
+            Sign In
+          </button>
+          <p className="relative mt-2 flex items-center">
+            <input type="checkbox" className="mr-2" />
             Remember Me
           </p>
-          <p>Not a member? Sign up now</p>
+          <p className="relative mt-8 text-center">Not a member? Sign up now</p>
         </form>
       </div>
     </div>
