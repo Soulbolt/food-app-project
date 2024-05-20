07548c3d8c8bf48777d65202d20b19ba4d333bfb
Soulbolt <soulbolt@gmail.com>
2024-05-20 10:14:40 -0700

    Add shadow to the form and img, Add img to left of form when screen size is over md and scale with it.

--- frontend/src/components/Login3.jsx
+++ frontend/src/components/Login3.jsx
@@ -1,24 +1,26 @@
 import React from "react";
 import kabobs from "../assets/kabobs.jpg";
+
 function Login3() {
   return (
     <div className="flex h-screen w-full">
-      <div className="md:grid-cols2 grid grid-cols-1">
-        <div className="hidden md:block">
-          <img src={kabobs} alt="kabobs" />
+      <div className="md:grid-cols2 m-auto grid h-[550px] grid-cols-1 shadow-lg shadow-gray-600 sm:max-w-[900px]">
+        <div className="hidden h-[550px] w-full md:block">
+          <img className="h-full w-full" src={kabobs} alt="kabobs" />
         </div>
-      </div>
-      <div>
-        <form>
-          <h2>My Food</h2>
-          <div>
-            <input type="text" placeholder="Username" />
-            <input type="password" placeholder="Password" />
-          </div>
-          <button>Sign In</button>
-          <p>Forgot Username or Password?</p>
-        </form>
-        <p>Don't have an account? Sign Up Here!</p>
+
+        <div>
+          <form>
+            <h2>My Food</h2>
+            <div>
+              <input type="text" placeholder="Username" />
+              <input type="password" placeholder="Password" />
+            </div>
+            <button>Sign In</button>
+            <p>Forgot Username or Password?</p>
+          </form>
+          <p>Don't have an account? Sign Up Here!</p>
+        </div>
       </div>
     </div>
   );
