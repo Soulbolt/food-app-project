e2c2a5ccb9017ee697a323d0725708556963ff74
Soulbolt <soulbolt@gmail.com>
2024-05-20 10:27:37 -0700

    Add styles to div wraps around form element, h2 title, inputs, and sign in button to have a uniform look and fill of a log in screen.

--- frontend/src/components/Login3.jsx
+++ frontend/src/components/Login3.jsx
@@ -4,19 +4,29 @@ import kabobs from "../assets/kabobs.jpg";
 function Login3() {
   return (
     <div className="flex h-screen w-full">
-      <div className="md:grid-cols2 m-auto grid h-[550px] grid-cols-1 shadow-lg shadow-gray-600 sm:max-w-[900px]">
+      <div className="m-auto grid h-[550px] grid-cols-1 shadow-lg shadow-gray-600 sm:max-w-[900px] md:grid-cols-2">
         <div className="hidden h-[550px] w-full md:block">
           <img className="h-full w-full" src={kabobs} alt="kabobs" />
         </div>
 
-        <div>
+        <div className="flex flex-col justify-around p-4">
           <form>
-            <h2>My Food</h2>
+            <h2 className="mb-8 text-center text-4xl font-bold">My Food</h2>
             <div>
-              <input type="text" placeholder="Username" />
-              <input type="password" placeholder="Password" />
+              <input
+                className="mr-2 border p-2"
+                type="text"
+                placeholder="Username"
+              />
+              <input
+                className="border p-2"
+                type="password"
+                placeholder="Password"
+              />
             </div>
-            <button>Sign In</button>
+            <button className="my-4 w-full bg-green-600 py-2 hover:bg-green-500">
+              Sign In
+            </button>
             <p>Forgot Username or Password?</p>
           </form>
           <p>Don't have an account? Sign Up Here!</p>
