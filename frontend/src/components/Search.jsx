68908b86f71a85cd310401c9da1ee0bd927a97ef
Cesar Cervantes <soulbolt@gmail.com>
2024-05-28 09:21:58 -0700

    Add Search component with variables for sticky using state, useRef and useEffect. Add handleScroll function to set sticky state. Add use effect to add and remove eventListener.

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,4 +1,20 @@
-function Search() {
+import React, { useState, useRef, useEffect } from 'react'
+
+function SearchBar() {
+    const [isSticky, setIsSticky] = useState(false);
+    const searchBarRef = useRef(null);
+
+    const handleScroll = () => {
+        const scrollTop = window.scrollY;
+        const searchBarHeight = searchBarRef.current.offsetHeight || 0;
+        setIsSticky(scrollTop > searchBarHeight);
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => { window.removeEventListener('scroll', handleScroll); };
+    }, []);
+
     return (
         <div>
             
@@ -6,4 +22,4 @@ function Search() {
     )
 }
 
-export default Search
+export default SearchBar
