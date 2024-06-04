9d4c8841d3f5d76fc2cf14adcad2842f88c87d38
Soulbolt <soulbolt@gmail.com>
2024-06-04 09:09:07 -0700

    Add a create sql statement if table does not exist, with id as primary key, name, address, contact number and rating as column attributes.

--- init-db/database.sql
+++ init-db/database.sql
@@ -1,1 +1,10 @@
 CREATE SCHEMA IF NOT EXISTS restaurants_database;
+
+CREATE TABLE IF NOT EXISTS restaurants_database.restaurants
+(
+    id          BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name        VARCHAR(255) NOT NULL,
+    address TEXT NOT NULL,
+    contact_number VARCHAR(255) NOT NULL,
+    rating DECIMAL(2, 1) DEFAULT NULL
+);
