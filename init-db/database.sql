aaf3ccfd5e35e23e8cc5d0e3637984555b1c1b19
Soulbolt <soulbolt@gmail.com>
2024-06-04 09:45:39 -0700

    Add function to generate random restaurant names and sddresses.

--- init-db/database.sql
+++ init-db/database.sql
@@ -1,5 +1,7 @@
+-- Create database
 CREATE SCHEMA IF NOT EXISTS restaurants_database;
 
+-- Create restaurants table
 CREATE TABLE IF NOT EXISTS restaurants_database.restaurants
 (
     id          BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -9,6 +11,7 @@ CREATE TABLE IF NOT EXISTS restaurants_database.restaurants
     rating DECIMAL(2, 1) DEFAULT NULL
 );
 
+-- Create reviews table
 CREATE TABLE IF NOT EXISTS restaurants_database.reviews
 (
     id          BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -18,3 +21,10 @@ CREATE TABLE IF NOT EXISTS restaurants_database.reviews
     rating DECIMAL(2, 1) NOT NULL,
     FOREIGN KEY (restaurant_id) REFERENCES restaurants_database.restaurants (id)
 )
+
+-- Function to generate random restaurants names and addresses
+CREATE OR REPLACE FUNCTION generate_random_string(length INTEGER DEFAULT 20)
+RETURNS VARCHAR(length) AS $$
+SELECT floor(random() * (26^length))::text || repeat('a', length);
+$$ LANGUAGE plpgsql VOLATILE COST 100;
+$$
