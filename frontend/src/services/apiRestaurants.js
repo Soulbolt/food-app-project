db031cba756809228d73db518dc93cbf58138879
Soulbolt <soulbolt@gmail.com>
2024-06-09 11:52:20 -0700

    Add APi call to retrieve  recommended restaurants list from the backend server.

--- frontend/src/services/apiRestaurants.js
+++ frontend/src/services/apiRestaurants.js
@@ -16,3 +16,20 @@ export async function fetchRestaurants() {
     console.log("Error fetching data", error);
   }
 }
+
+export async function fetchRecommendedRestaurants() {
+  try {
+    const response = await axios.get("/api/restaurants/recommended");
+    const restaurantList = response.data.map((restaurant) => {
+      return {
+        ...restaurant,
+        isFavorite: false,
+        id: restaurant.id,
+      };
+    });
+    console.log("The list:", restaurantList);
+    return restaurantList;
+  } catch (error) {
+    console.log("Error fetching data", error);
+  }
+}
