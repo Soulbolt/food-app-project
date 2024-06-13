81c8eda181f17982d910536cc1f45d7bd4ebe52d
Soulbolt <soulbolt@gmail.com>
2024-06-13 13:27:43 -0700

    Add functions to handle toggleDropdown and categorySelect, so that the dropdown menu display when click on the Show all button and change button value to the selected option.

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,26 +1,34 @@
-import React from "react";
-import { GoSearch } from "react-icons/go";
+import React, { useState } from "react";
 
 function SearchBar({ search, setSearch, handleSearch, error }) {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  const toggleDropdown = () => {
+    setDropdownOpen((dropdownOpen) => !dropdownOpen);
+    console.log("toggled", dropdownOpen.toString());
+  };
+
+  const handleCategorySelect = (event) => {
+    setSelectedCategory(event);
+  };
+
   return (
     <div className="relative">
       <form onSubmit={handleSearch} className="mx-auto max-w-lg">
-        <div class="flex">
-          <label
-            for="search-dropdown"
-            class="sr-only mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
+        <div className="flex">
+          <label className="sr-only mb-2 text-sm font-medium text-gray-900 dark:text-white">
             Your Email
           </label>
           <button
             id="dropdown-button"
-            data-dropdown-toggle="dropdown"
-            class="z-10 inline-flex flex-shrink-0 items-center rounded-s-lg border border-gray-300 bg-gray-100 px-4 py-2.5 text-center text-sm font-medium text-gray-900 hover:bg-gray-200 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-700"
+            onClick={toggleDropdown}
+            className="z-60 inline-flex flex-shrink-0 items-center rounded-s-lg border border-gray-300 bg-gray-100 px-4 py-2.5 text-center text-sm font-medium text-gray-900 hover:bg-gray-200 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-700"
             type="button"
           >
-            All categories{" "}
+            {selectedCategory || "All categories"}
             <svg
-              class="ms-2.5 h-2.5 w-2.5"
+              className="ms-2.5 h-2.5 w-2.5"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -28,71 +36,49 @@ function SearchBar({ search, setSearch, handleSearch, error }) {
             >
               <path
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="m1 1 4 4 4-4"
               />
             </svg>
           </button>
           <div
             id="dropdown"
-            class="z-10 hidden w-44 divide-y divide-gray-100 rounded-lg bg-white shadow dark:bg-gray-700"
+            className="z-10 w-44 divide-y divide-gray-100 rounded-lg bg-white shadow dark:bg-gray-700"
           >
-            <ul
-              class="py-2 text-sm text-gray-700 dark:text-gray-200"
-              aria-labelledby="dropdown-button"
-            >
-              <li>
-                <button
-                  type="button"
-                  class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                >
+            {dropdownOpen && (
+              <ul
+                className="py-2 text-sm text-gray-700 dark:text-gray-200"
+                aria-labelledby="dropdown-button"
+              >
+                <li onClick={() => handleCategorySelect("Show All")}>
                   Show All
-                </button>
-              </li>
-              <li>
-                <button
-                  type="button"
-                  class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                >
-                  By Name
-                </button>
-              </li>
-              <li>
-                <button
-                  type="button"
-                  class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                >
+                </li>
+                <li onClick={() => handleCategorySelect("By Name")}>By Name</li>
+                <li onClick={() => handleCategorySelect("Category")}>
                   Category
-                </button>
-              </li>
-              <li>
-                <button
-                  type="button"
-                  class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                >
-                  By Id
-                </button>
-              </li>
-            </ul>
+                </li>
+                <li onClick={() => handleCategorySelect("By Id")}>By Id</li>
+              </ul>
+            )}
           </div>
-          <div class="relative w-full">
+          <div className="relative w-full">
             <input
               type="search"
               id="search-dropdown"
               value={search || ""}
               onChange={(e) => setSearch(e.target.value)}
-              class="z-20 block w-full rounded-e-lg border border-s-2 border-gray-300 border-s-gray-50 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:border-s-gray-700  dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500"
+              className="z-20 block w-full rounded-e-lg border border-s-2 border-gray-300 border-s-gray-50 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:border-s-gray-700  dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500"
               placeholder="Search Mockups, Logos, Design Templates..."
               required
             />
             <button
               type="submit"
-              class="absolute end-0 top-0 h-full rounded-e-lg border border-blue-700 bg-blue-700 p-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="absolute end-0 top-0 h-full rounded-e-lg border border-blue-700 bg-blue-700 p-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               <svg
-                class="h-4 w-4"
+                className="h-4 w-4"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -100,13 +86,13 @@ function SearchBar({ search, setSearch, handleSearch, error }) {
               >
                 <path
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
                 />
               </svg>
-              <span class="sr-only">Search</span>
+              <span className="sr-only">Search</span>
             </button>
           </div>
         </div>
