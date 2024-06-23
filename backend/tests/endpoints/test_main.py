77c7bbb3b4a5e04d7c0060c8135b8d9ba0cc0869
Cesar Cervantes <soulbolt@gmail.com>
2024-06-23 11:11:03 -0700

    Add test_read_restaurant_by_id with client and db_session as parameters. Use for loop to create restaurants in memory. Add responses for successful and unsuccessful status code and json response.

--- backend/tests/endpoints/test_main.py
+++ backend/tests/endpoints/test_main.py
@@ -75,3 +75,45 @@ def test_read_restaurants_by_name(client, db_session):
     assert response.status_code == 404
     assert response.json() == {"detail": "Restaurant not found"}
 
+@pytest.fixture(scope="module")
+def test_read_restaurants_by_id(client, db_session):
+    for i in range(10):
+        restaurant = Restaurant(
+            name=f"restaurant_{i}",
+            address=f"address_{i}",
+            contact_number=f"contact_number_{i}",
+            rating=i,
+        )
+        db_session.add(restaurant)
+    db_session.commit()
+    yield
+
+    response = client.get("/api/restaurant/1")
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": 1,
+        "name": "restaurant_0",
+        "address": "address_0",
+        "contact_number": "contact_number_0",
+        "rating": 0,
+        "reviews": [],
+    }
+
+    response = client.get("/api/restaurant/5")
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": 5,
+        "name": "restaurant_4",
+        "address": "address_4",
+        "contact_number": "contact_number_4",
+        "rating": 4,
+        "reviews": [],
+    }
+
+    response = client.get("/api/restaurant/10")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Restaurant not found"}
+
+    response = client.get("/api/restaurant/11")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Restaurant not found"}
