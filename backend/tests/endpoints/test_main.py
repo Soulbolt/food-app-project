9f14a9f8637af932c1bd2d2eacef12ca781bf7cb
Soulbolt <soulbolt@gmail.com>
2024-06-25 10:55:47 -0700

    Refactor test cases to use local mock data.

--- backend/tests/endpoints/test_main.py
+++ backend/tests/endpoints/test_main.py
@@ -1,5 +1,4 @@
 import pytest
-import sqlite3
 import os
 import sys
 # Add the parent directory to the sys.path
@@ -8,15 +7,9 @@ from main import app, connect_to_database
 from mock_data.mock_restaurants_db import DB
 from fastapi.testclient import TestClient
 
-# Mock database in json format
-db = DB
-
 # Override the connect_to_database function to return an in-memory database
 def override_connect_to_database():
-    conn = sqlite3.connect(DB, uri=True)
-    conn.row_factory = sqlite3.Row
-    yield conn
-    conn.close()
+    return DB
 
 app.dependency_overrides[connect_to_database] = override_connect_to_database
 
@@ -34,45 +27,42 @@ def client():
 
 # Test the get_restaurants endpoint with status code 200
 def test_get_all_restaurants(client):
-    mock_db = client.app.state.database = {"restaurants": db}
-    if mock_db:
-        response = client.get("/api/restaurants")
-        print(response.json())
-        assert response.status_code == 200
-        assert len(response.json()) > 0
+    response = client.get("/api/restaurants")
+    print("data: ", response.json()) # Print the response for debugging
+    assert response.status_code == 200
+    assert len(response.json()) > 0 # Check if the response is not empty
 
 # Test the get_restaurants endpoint with status code 404
 def test_get_restaurants_not_found(client):
-    mock_db = client.app.state.database = {"restaurants": []}
-    if not mock_db:
-        raise Exception("Could not connect to the database")
     response = client.get("/api/restaurants")
+    print("data: ", response.json()) # Print the response for debugging
     assert response.status_code == 404
     assert response.json() == {"detail": "Restaurant not found"}
 
 # Test the get_restaurants_by_name endpoint with status code 200
-def test_get_restaurants_by_name(client):
-    response = client.get("/api/restaurants_by_name/golden")
+def test_get_restaurants_by_name(client, name="golden"):
+    response = client.get(f"/api/restaurants_by_name/{name}")
+    print("data: ", response.json()) # Print the response for debugging
     assert response.status_code == 200
-    assert len(response.json()) > 0
+    assert len(response.json()) > 0 # Check if the response is not empty
 
 # Test the get_restaurants_by_name endpoint with status code 500
-def test_get_restaurants_by_name_not_found(client, name="not_found"):
-    mock_db = client.app.state.database = {"restaurants": DB}
-    if name not in mock_db:
-        response = client.get(f"/api/restaurants_by_name/{name}")
-        print("response", response.json())
-        assert response.status_code == 500
-        assert response.json() == {"detail": "500: Restaurant not found"}
+def test_get_restaurants_by_name_not_found(client, name="golden"):
+    response = client.get(f"/api/restaurants_by_name/{name}")
+    print("data: ", response.json())
+    assert response.status_code == 500
+    assert response.json() == {"detail": "500: Restaurant not found"}
 
 # Test the get_restaurant_by_id endpoint with status code 200
 def test_get_restaurant_by_id(client):
     response = client.get("/api/restaurant/1")
+    print("data: ", response.json()) # Print the response for debugging
     assert response.status_code == 200
     assert response.json()["name"] == "The Golden Gate Grill"
 
 # Test the get_recommended_restaurants endpoint with status code 200
 def test_get_recommended_restaurants(client):
     response = client.get("/api/restaurants/recommended")
+    print("data: ", response.json()) # Print the response for debugging
     assert response.status_code == 200
     assert len(response.json()) > 0
