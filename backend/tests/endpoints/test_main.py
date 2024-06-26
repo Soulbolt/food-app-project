668417139e75430fe5de7094714279724805799d
Soulbolt <soulbolt@gmail.com>
2024-06-26 11:55:05 -0700

    Add test case for create a retaurant.

--- backend/tests/endpoints/test_main.py
+++ backend/tests/endpoints/test_main.py
@@ -1,22 +1,26 @@
+import sqlite3
 import pytest
 import os
 import sys
+
 # Add the parent directory to the sys.path
 sys.path.append(os.path.join(os.path.dirname(__file__), "../../src"))
-from main import app
+from main import app, connect_to_database
 from fastapi.testclient import TestClient
 
-# Create a client for testing
+# Override the connect_to_database function to use the existing database
+def override_connect_to_database():
+    conn = sqlite3.connect('restaurants.db')
+    conn.row_factory = sqlite3.Row
+    yield conn
+    conn.close()
+
+app.dependency_overrides[connect_to_database] = override_connect_to_database
+
+# Create a test client for testing
 @pytest.fixture
 def client():
     return TestClient(app)
-
-# Connect to the mock database
-# @pytest.fixture
-# def connection():
-#     conn = sqlite3.connect(DB) # In-memory database for testing
-#     yield conn
-#     conn.close()
 
 # Test the get_restaurants endpoint with status code 200
 def test_get_all_restaurants(client):
@@ -27,13 +31,26 @@ def test_get_all_restaurants(client):
 
 # Test the get_restaurants endpoint with status code 404
 def test_get_restaurants_not_found(client):
-    response = client.get("/api/restaurants/101")
+    response = client.get("/api/restaurants/10")
     print("data: ", response.json()) # Print the response for debugging
     assert response.status_code == 404
     assert response.json() == {"detail": "Not Found"}
 
+# Test create a new restaurant with status code 201
+def test_create_restaurant(client):
+    new_restaurant = {
+        "name": "New Test Restaurant",
+        "address": "123 Main St",
+        "contact_number": "555-555-5555",
+        "rating": 4.5
+    }
+    response = client.post("/api/restaurants", json=new_restaurant)
+    print("data: ", response.json()) # Print the response for debugging
+    assert response.status_code == 201
+    assert response.json()["name"] == new_restaurant["name"]
+
 # Test the get_restaurants_by_name endpoint with status code 200
-def test_get_restaurants_by_name(client, name="golden"):
+def test_get_restaurants_by_name(client, name="not found"):
     response = client.get(f"/api/restaurants_by_name/{name}")
     print("data: ", response.json()) # Print the response for debugging
     assert response.status_code == 200
@@ -51,7 +68,7 @@ def test_get_restaurant_by_id(client):
     response = client.get("/api/restaurant/1")
     print("data: ", response.json()) # Print the response for debugging
     assert response.status_code == 200
-    assert response.json()["name"] == "The Golden Gate Grill"
+    assert response.json()["name"] == "The Gate Grill"
 
 # Test the get_restaurant_by_id endpoint with status code 500
 def test_get_retaurant_by_id_not_found(client):
