ab0ee60001a6413649c24edba79a13aec3519cdf
Soulbolt <soulbolt@gmail.com>
2024-06-29 13:36:30 -0700

    Add async function to query PATCH statement to update restaurant by id, Add error handling for connection and if id does not exists.

--- backend/src/main.py
+++ backend/src/main.py
@@ -209,7 +209,7 @@ async def get_restaurants_by_name(restaurant_name: str):
 
 """ Returns the restaurant with the specified ID """
 @app.get("/api/restaurant/{id}", response_model=Restaurant)
-def get_restaurant(id: int):
+async def get_restaurant(id: int):
     conn = connect_to_database()
     if not conn:
         raise HTTPException(status_code=500, detail="Could not connect to the database")
@@ -270,7 +270,7 @@ def get_restaurant(id: int):
 
 """ Returns the recommended list of restaurants """
 @app.get("/api/restaurants/recommended", response_model=list[Restaurant])
-def get_recommended_restaurants():
+async def get_recommended_restaurants():
     restaurant_list = [
         {
             "id": restaurant["id"],
@@ -291,18 +291,39 @@ def get_restaurant(id: int):  # noqa: F811
         if restaurant["id"] == id:
             return restaurant
 
-""" Updates the restaurant with the specified ID """
-@app.put("/api/update_restaurant/{id}")
-def update_restaurant(id: int, restaurant: Restaurant):
+""" Updates the restaurant with the specified ID in mock database """
+@app.put("/api/mock_update_restaurant/{id}", response_model=Restaurant)
+async def update_restaurant_mock(id: int, restaurant: Restaurant):
     for restaurant in DB:
         if restaurant["id"] == id:
             restaurant.update(restaurant)
     return DB
 
+""" Updates the restaurant with the specified ID """
+@app.patch("/api/update_restaurant/{id}", response_model=Restaurant)
+async def update_restaurant(id: int, restaurant: Restaurant):
+    conn = connect_to_database()
+    if not conn:
+        raise HTTPException(status_code=500, detail="Could not connect to the database")
+    try:
+        print("Connected to the database!")
+        cursor = conn.cursor()
+        query = "UPDATE restaurant_schema.restaurants SET category = %s, name = %s, address = %s, contact_number = %s, rating = %s, is_favorite = %s WHERE id = %s;"
+        cursor.execute(query, (restaurant.category, restaurant.name, restaurant.address, restaurant.contact_number, restaurant.rating, restaurant.isFavorite, id))
+        if cursor.rowcount == 0:
+            raise HTTPException(status_code=500, detail=f"Restaurant with specified ID {id} not found")
+        conn.commit()
+        return {"message": "Restaurant updated successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    finally:
+        cursor.close()
+        conn.close()
+
 """ Deletes the restaurant with the specified ID """
   # TODO: Consider and define business logic for deleting a restaurant in relation to reviews.
 @app.delete("/api/delete_restaurant/{id}")
-def delete_restaurant(id: int):
+async def delete_restaurant(id: int):
     conn = connect_to_database()
     if not conn:
         raise HTTPException(status_code=500, detail="Could not connect to the database")
