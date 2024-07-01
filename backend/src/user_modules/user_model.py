66166b4b373ee124901fe7e234883a119dace9ae
Cesar Cervantes <soulbolt@gmail.com>
2024-07-01 13:57:12 -0700

    Add id with type int and email, password of type str along with favorites type list to complete the user Model.

--- backend/src/user_modules/user_model.py
+++ backend/src/user_modules/user_model.py
@@ -1,7 +1,12 @@
 from pydantic import BaseModel
 
-""" Pydantic model for User data """
+""" Pydantic model for User data and Favorites """
 class User(BaseModel):
+    id: int
+    email: str
+    password: str
     username: str
     name: str
+    favorites: []
+
 
