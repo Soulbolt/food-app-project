16b41ed0377c99dbdaed5565299b733529b69de3
Cesar Cervantes <soulbolt@gmail.com>
2024-07-03 14:43:14 -0700

    Add classes BaseModel structures: UserCredentials, userInDB, UserInResponse, UserInResponseWithToken, CreateUser

--- backend/src/user_modules/user_model.py
+++ backend/src/user_modules/user_model.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from restaurant import Restaurant, Review
+from restaurant_modules.restaurant import Restaurant, Review
 
 """ Pydantic model for User data and Favorites """
 class Favorite(BaseModel):
@@ -10,7 +10,7 @@ class Favorite(BaseModel):
     rating: float
     is_favorite: bool
     reviews: list[Review] = []
-    
+
 class User(BaseModel):
     id: int
     email: str
@@ -18,4 +18,37 @@ class User(BaseModel):
     username: str
     name: str
     favorites: list[Favorite] = []
+
+class UserCredentials(BaseModel):
+    email: str
+    password: str
+
+class UserInDB(User):
+    hashed_password: str
+    class Config:
+        orm_mode = True
+
+class UserInResponse(BaseModel):
+    id: int
+    email: str
+    username: str
+    name: str
+    favorites: list[Favorite]
+    class Config:
+        orm_mode = True
+
+class UserInResponseWithToken(UserInResponse):
+    access_token: str
+    token_type: str
+    class Config:
+        orm_mode = True
+
+class CreateUser(BaseModel):
+    email: str
+    password: str
+    username: str
+    name: str
+    class Config:
+        orm_mode = True
+
 
