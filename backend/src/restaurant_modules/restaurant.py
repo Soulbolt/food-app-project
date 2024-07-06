80d11030f8cb3d6114c4effe9f46667632077513
Cesar Cervantes <soulbolt@gmail.com>
2024-07-05 19:21:10 -0700

    Update restaurant models for sqlalchemy structure and pydantc format for serialization and deserialization.

--- backend/src/restaurant_modules/restaurant.py
+++ backend/src/restaurant_modules/restaurant.py
@@ -1,10 +1,10 @@
+from pydantic import BaseModel
 from sqlalchemy import Column, Integer, String, Float, Boolean, ForeignKey
-from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, declarative_base
 
 Base = declarative_base()
 # Base class for ORM models, I want to make sure that the relationship between the Restaurant and Review models is properly defined.
-""" Pydantic models for Restaurant and Review data """
+""" SqlAlchemy table models for Restaurant and Review data """
 class Review(Base):
     __tablename__ = 'reviews'
     id = Column(Integer, primary_key=True, index=True)
@@ -26,3 +26,27 @@ class Restaurant(Base):
     is_favorite= Column(Boolean)
     # Define the relationship between the Restaurant and Review models
     reviews= relationship("Review", back_populates="restaurant")
+
+""" Pydantic models for Restaurant and Review data """
+class ReviewModel(BaseModel):
+    # id: int
+    username: str
+    review: str
+    rating: float
+    # restaurant_id: int
+
+    model_config = {}
+    model_config['from_attributes'] = True
+
+class RestaurantModel(BaseModel):
+    id: int
+    category: str
+    name: str
+    address: str
+    contact_number: str
+    rating: float
+    is_favorite: bool
+    reviews: list[ReviewModel] = []
+
+    model_config = {}
+    model_config['from_attributes'] = True
