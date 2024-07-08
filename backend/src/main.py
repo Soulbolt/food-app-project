6e90911a420eb4f94a51b3610d7109df2c32fd3e
Cesar Cervantes <soulbolt@gmail.com>
2024-07-08 15:12:23 -0700

    refactor some endpoint functions to produce expected results, removed unnecessary commended out code.

--- backend/src/main.py
+++ backend/src/main.py
@@ -11,10 +11,10 @@ from sqlalchemy.orm import sessionmaker, Session, joinedload
 from sqlalchemy.exc import SQLAlchemyError
 import bcrypt
 
-from mock_data.mock_restaurants_db import DB
-from restaurant_modules.restaurant import Restaurant, RestaurantModel, ReviewModel
+from restaurant_modules.restaurant import Restaurant, RestaurantModel, ReviewModel, RestaurantCreate
 from user_modules.user_model import User, Base
 from mock_data.mock_users_db import USER_DB
+from mock_data.mock_restaurants_db import DB
 import logging
 
 # Load environment variables
@@ -65,38 +65,18 @@ app.add_middleware(
 #     'port': os.getenv('DB_PORT'),
 # }
 
-def get_database_connection_string():
-    print("inside get_database_connection_string: ")
-    # Check if we are in a testing environment
-    if os.getenv("ENV") == 'test':
-        database_url = os.getenv('TEST_DATABASE_URL')
-    else:
-        database_url = os.getenv('DATABASE_URL')
-    
-    if database_url and "5432" in database_url:
-        database_url = database_url.replace(":5432", ":5433")
-
-    return database_url
-
+#     return database_url
+print("DATABASE_URL: ", DATABASE_URL)
 def connect_to_database():
-    database_url = get_database_connection_string()
+    # database_url = get_database_connection_string()
+    database_url = os.getenv("TEST_DATABASE_URL")
     try:
         if database_url:
             print(f"Connecting to the database at {database_url}...")
             engine = create_engine(database_url)
             conn = engine.connect()
             return conn
-        # if database_url and database_url.startswith("postgres://"):
-        #     print("Connecting to the PostgreSQL database...")
-            # Establish connection
-            # conn = psycopg2.connect(
-            #     host=db_config['host'],
-            #     dbname=db_config['dbname'],
-            #     user=db_config['user'],
-            #     password=db_config['password'],
-            #     port=db_config['port']
-            # )
-            # return conn
+
         elif database_url and database_url.startswith("sqlite://"):
             # Establish connection
             print("Connecting to the SQLite database...")
@@ -159,49 +139,22 @@ def authenticate_user(username: str, password: str, db: Session = Depends(get_db
 #* ------------- Restaurant CRUD REST APIs ---------------------- ###
 
 """ Creates a new restaurant """
-@app.post("/api/new_restaurant", response_model=RestaurantModel)
-def create_restaurant(restaurant_data, db: Session = Depends(get_db)):
+@app.post("/api/new_restaurant")
+def create_restaurant(restaurant_data: RestaurantCreate, db: Session = Depends(get_db)):
     try:
-        db_restaurant = Restaurant(**restaurant_data)
+        db_restaurant = Restaurant(**restaurant_data.model_dump())
         db.add(db_restaurant)
         db.commit()
         db.refresh(db_restaurant)
-        return RestaurantModel.model_validate(db_restaurant)
+        return JSONResponse(content={"message": "Restaurant created successfully!"})
     except SQLAlchemyError as e:
         print("Error creating restaurant: ", e)
         raise HTTPException(status_code=500, detail="Database transaction failed")
     finally:
         db.close()
-    # conn = connect_to_database()
-    # if not conn:
-    #     raise HTTPException(status_code=500, detail="Could not connect to the database")
-    # try:
-    #     print("Connected to the database!")
-    #     cursor = conn.cursor()
-    #     query = """
-    #         INSERT INTO restaurant_schema.restaurants (category, name, address, contact_number, rating)
-    #         VALUES (%s, %s, %s, %s, %s);
-    #     """
-    #     cursor.execute(query, (restaurant.category, restaurant.name, restaurant.address, restaurant.contact_number, restaurant.rating))
-    #     conn.commit()
-    #     return JSONResponse(content={"message": "Restaurant created successfully!"})
-    # except Exception as e:
-    #     raise HTTPException(status_code=500, detail=str(e))
-    # finally:
-    #     cursor.close()
-    #     conn.close()
-
-""" Creates a new restaurant in Mock Data """
-# @app.post("/api/mock_restaurant", response_model=RestaurantModel)
-# async def create_mock_restaurant(restaurant: Restaurant):
-#     try:
-#         DB.append(restaurant)
-#         return RestaurantModel.from_orm(restaurant)
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e))
         
 """ Returns the entire list of restaurants or return restaurants based on the local_kw query parameter"""
-@app.get("/api/restaurants/", response_model=list[RestaurantModel])
+@app.get("/api/restaurants/{name}", response_model=list[RestaurantModel])
 def get_restaurants(name: Optional[str] = None, skip: int = 0, limit: int = 150, db: Session = Depends(get_db)):
     logging.info(f"Querying restaurants with name: {name}, skip: {skip}, limit: {limit}")
     query = db.query(Restaurant)
@@ -233,76 +186,6 @@ def get_restaurants_by_name(name: str, db: Session = Depends(get_db)):
         raise HTTPException(status_code=500, detail="Error fetching restaurants")  
     finally:
         db.close()
-    # conn = connect_to_database()
-    # if not conn:
-    #     raise HTTPException(status_code=500, detail="Could not connect to the database")
-    # try:
-    #     print("Connected to the database!")
-    #     cursor = conn.cursor()
-    #     query = """
-    #         SELECT 
-    #             r.id AS id,
-    #             r.category,
-    #             r.name AS name,
-    #             r.address,
-    #             r.contact_number,
-    #             r.rating AS rating,
-    #             r.is_favorite,
-    #             COALESCE(
-    #                 JSON_AGG(
-    #                     JSON_BUILD_OBJECT(
-    #                         'username', rv.username,
-    #                         'review', rv.review,
-    #                         'rating', rv.rating
-    #                     )
-    #                 ) FILTER (WHERE rv.username IS NOT NULL), '[]'
-    #             ) AS reviews
-    #         FROM 
-    #             restaurant_schema.restaurants r
-    #         LEFT JOIN 
-    #             restaurant_schema.reviews rv ON r.id = rv.restaurant_id
-    #         WHERE
-    #             LOWER(r.name) LIKE %(restaurant_name)s
-    #         GROUP BY
-    #             r.id, r.category, r.name, r.address, r.contact_number, r.rating, r.is_favorite
-    #         ORDER BY r.name ASC;
-    #     """
-    #     # Convert restaurant_name to lowercase for case-insensitive search
-    #     restaurant_name = restaurant_name.lower()
-    #     # Replace spaces with % for SQL wildcard search
-    #     restaurant_name = restaurant_name.replace(" ", "%")
-    #     cursor.execute(query, {"restaurant_name": f'%{restaurant_name}%'})
-    #     rows = cursor.fetchall()
-    #     if rows is None:
-    #         raise HTTPException(status_code=404, detail="Restaurant not found")
-        
-    #     if cursor.description is not None:
-    #         [desc[0] for desc in cursor.description]
-        
-    #     restaurant_list = []
-    #     for row in rows:
-    #         restaurant_data = Restaurant(
-    #             id= row[0],
-    #             category= row[1],
-    #             name= row[2],
-    #             address= row[3],
-    #             contact_number= row[4],
-    #             rating= float(row[5]), # Convert Decimal to float for JSON serialization
-    #             is_favorite= bool(row[6]), # Convert boolean to bool for JSON serialization
-    #             reviews= row[7] if row[7] else [] # Use the aggregated JSON for reviews
-    #         )
-
-    #         restaurant_list.append(restaurant_data)
-
-    #     if restaurant_list == []:
-    #         raise HTTPException(status_code=500, detail="Restaurant not found")
-
-    #     return restaurant_list
-    # except Exception as e:
-    #     raise HTTPException(status_code=500, detail=str(e))
-    # finally:
-    #     cursor.close()
-    #     conn.close()
 
 """ Returns the restaurant with the specified ID """
 @app.get("/api/restaurant/{id}", response_model=RestaurantModel)
@@ -317,68 +200,14 @@ def get_restaurant_by_id(id: int, db: Session=(Depends(get_db)) ):
         raise HTTPException(status_code=500, detail="list index out of range")
     finally:
         db.close()
-    # conn = connect_to_database()
-    # if not conn:
-    #     raise HTTPException(status_code=500, detail="Could not connect to the database")
-    # try:
-    #     print("Connected to the database!")
-    #     cursor = conn.cursor()
-    #     query = """
-    #         SELECT 
-    #             r.id AS id,
-    #             r.category,
-    #             r.name AS name,
-    #             r.address,
-    #             r.contact_number,
-    #             r.rating AS rating,
-    #             r.is_favorite,
-    #             rv.username,
-    #             rv.review,
-    #             rv.rating AS rating
-    #         FROM 
-    #             restaurant_schema.restaurants r
-    #         LEFT JOIN 
-    #             restaurant_schema.reviews rv ON r.id = rv.restaurant_id
-    #         WHERE r.id = %s;
-    #     """
-    #     cursor.execute(query, (id,))
-    #     rows = cursor.fetchall()
-    #     if rows is None:
-    #         raise HTTPException(status_code=404, detail="Restaurant not found")
-        
-    #     if cursor.description is not, None:
-    #         [desc[0] for desc in cursor.description]
-        
-    #     restaurant_data = Restaurant(
-    #         id=rows[0][0],
-    #         category=rows[0][1],
-    #         name=rows[0][2],
-    #         address=rows[0][3],
-    #         contact_number=rows[0][4],
-    #         rating=float(rows[0][5]), # Convert Decimal to float for JSON serialization
-    #         is_favorite=bool(rows[0][6]), # Convert boolean to bool for JSON serialization
-    #         reviews=[]
-    #     )
-        
-    #     for row in rows:
-    #         if row[5]: # Check if there is a review for this restaurant
-    #             review = Review(
-    #                 username=row[7],
-    #                 review=row[8],
-    #                 rating=float(row[9]) # Convert Decimal to float for JSON serialization
-    #             )
-    #             restaurant_data.reviews.append(review)
-        
-    #     return restaurant_data
-    # except Exception as e:
-    #     raise HTTPException(status_code=500, detail=str(e))
-    # finally:
-    #     cursor.close()
-    #     conn.close()
 
 """ Returns the recommended list of restaurants """
-@app.get("/api/restaurants/recommended", response_model=list[dict[str, Any]])
+@app.get("/api/restaurants/recommended", response_model=list[RestaurantModel])
 def get_recommended_restaurants():
+    # Verify that the mock database is not empty
+    if not DB:
+        raise HTTPException(status_code=404, detail="No restaurants found")
+    
     restaurant_list = [
         {
             "id": restaurant.id,
@@ -394,27 +223,14 @@ def get_recommended_restaurants():
     ]
     return restaurant_list
 
-""" Returns the restaurant with the specified ID """
-# @app.get("/api/recommended_restaurant/{id}")
-# def get_restaurant(id: int):  # noqa: F811
-#     for restaurant in DB:
-#         if restaurant.id == id:
-#             return restaurant
-
-""" Updates the restaurant with the specified ID in mock database """
-# @app.put("/api/mock_update_restaurant/{id}", response_model=Restaurant)
-# async def update_restaurant_mock(id: int, restaurant: Restaurant):
-#     for restaurant in DB:
-#         if restaurant["id"] == id:
-#             restaurant.update(restaurant)
-#     return DB
-
 """ Updates the restaurant with the specified ID """
 @app.patch("/api/update_restaurant/{id}", response_model=RestaurantModel)
-def update_restaurant(id: int, updates, db: Session = Depends(get_db)):
+def update_restaurant(id: int, updates: RestaurantModel, db: Session = Depends(get_db)):
     try:
         db_restaurant = db.query(Restaurant).filter(Restaurant.id == id).first()
-        for key, value in updates.items():
+        if not db_restaurant:
+            raise HTTPException(status_code=404, detail="Restaurant not found")
+        for key, value in updates.model_dump().items():
             setattr(db_restaurant, key, value)
         db.commit()
         db.refresh(db_restaurant)
@@ -424,39 +240,6 @@ def update_restaurant(id: int, updates, db: Session = Depends(get_db)):
         raise HTTPException(status_code=500, detail="Database transaction failed")
     finally:
         db.close()
-    # conn = connect_to_database()
-    # if not conn:
-    #     raise HTTPException(status_code=500, detail="Could not connect to the database")
-    # try:
-    #     print("Connected to the database!")
-    #     cursor = conn.cursor()
-    #     existing_restaurant_query = "SELECT * FROM restaurant_schema.restaurants WHERE id = %s"
-    #     cursor.execute(existing_restaurant_query, [id,])
-    #     existing_restaurant = cursor.fetchone()
-    #     if not existing_restaurant:
-    #         raise HTTPException(status_code=404, detail="Restaurant not found")
-
-    #     # Convert the Row object to a dictionary
-    #     if cursor.description is not None:
-    #         column_names = [desc[0] for desc in cursor.description]
-    
-    #     existing_restaurant = dict(zip(column_names, existing_restaurant))
-
-    #     # Update the existing restaurant with the new data from the request
-    #     update_data = restaurant.model_dump(exclude_unset=True)
-    #     set_clause = ", ".join([f"{key} = %s" for key in update_data.keys()])
-    #     query = f"UPDATE restaurant_schema.restaurants SET {set_clause} WHERE id = %s"
-    #     values = list(update_data.values()) + [id]
-    #     cursor.execute(query, values)
-    #     conn.commit()
-    #     updated_restaurant = {**existing_restaurant, **update_data}
-    #     print("Updated restaurant:", updated_restaurant)
-    #     return updated_restaurant
-    # except Exception as e:
-    #     raise HTTPException(status_code=500, detail=str(e))
-    # finally:
-    #     cursor.close()
-    #     conn.close()
 
 """ Deletes the restaurant with the specified ID """
 # TODO: Consider and define business logic for deleting a restaurant in relation to reviews.
@@ -472,21 +255,3 @@ def delete_restaurant(id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=500, detail="Database transaction failed")
     finally:
         db.close()
-    # conn = connect_to_database()
-    # if not conn:
-    #     raise HTTPException(status_code=500, detail="Could not connect to the database")
-    # try:
-    #     print("Connected to the database!")
-    #     cursor = conn.cursor()
-    #     query = "DELETE FROM restaurant_schema.restaurants WHERE id = %s;"
-    #     cursor.execute(query, (id,))
-
-    #     if cursor.rowcount == 0:
-    #         raise HTTPException(status_code=500, detail=f"Restaurant with specified ID {id} not found")
-    #     conn.commit()
-    #     return {"message": "Restaurant deleted successfully"}
-    # except Exception as e:
-    #     raise HTTPException(status_code=500, detail=str(e))
-    # finally:
-    #     cursor.close()
-    #     conn.close()
